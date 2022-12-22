002b90935d5ac05b037ae19d8d238275e184eb97
Hedayatpour2002 <106871002+Hedayatpour2002@users.noreply.github.com>
2022-12-22 22:24:03 +0330

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+var meno = document.getElementById("mobile-menu");
+var icon = document.getElementById("mobile-menu-icon");
+var icon2 = document.getElementById("mobile-menu-icon-exit");
+
+function myFun() {
+  meno.style.left = "0px";
+}
+function myFun1() {
+  meno.style.left = "-800px";
+}
